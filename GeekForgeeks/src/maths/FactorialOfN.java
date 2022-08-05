24b297edafa86a39480d4f00823fd020ea5a4034
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-08-05 07:55:22 +0530

    adding recursion package

--- GeekForgeeks/src/maths/FactorialOfN.java
+++ GeekForgeeks/src/maths/FactorialOfN.java
@@ -8,10 +8,17 @@ public class FactorialOfN {
 		}
 		System.out.println(fact);
 	}
+	static int recusion(int n) {
+		if(n==0) 
+			return 1;
+		return n*recusion(n-1);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		fact(5);
+		int fact=recusion(5);
+		System.out.println(fact);
 
 	}
 

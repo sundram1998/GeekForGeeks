29504254d5d50f141ff09fd3c482fe8b8e360f4b
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-08-20 09:21:09 +0530

    add

--- GeekForgeeks/src/arrays/RemoveDublicateElement.java
+++ GeekForgeeks/src/arrays/RemoveDublicateElement.java
@@ -1,6 +1,22 @@
 package arrays;
 
 public class RemoveDublicateElement {
+	// complexity O(n) and space complexity Theta(1) 
+	static int removeDublicates(int[] arr) {
+		int n=arr.length;
+		int res=1;
+		for(int i=1;i<n;i++) {
+			if(arr[i]!=arr[res-1]) {
+				arr[res]=arr[i];
+				res++;
+			}
+		}
+		for(int i=0;i<res;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		return res;
+	}
+	// complexity O(n) and space complexity Theta(n)
 	static int removeDublicateElement(int[] arr) {
 		int n=arr.length;
 		int[] temp=new int[n];
@@ -21,7 +37,8 @@ public class RemoveDublicateElement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] array= {10,20,20,30,30,40};
-		System.out.println("and size is "+removeDublicateElement(array));
+		System.out.println(removeDublicates(array));
+		//System.out.println("and size is "+removeDublicateElement(array));
 
 	}
 

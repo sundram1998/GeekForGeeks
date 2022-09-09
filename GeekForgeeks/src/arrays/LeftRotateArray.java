2134ae647b3cffaa08a0783029ca8f4864f783cf
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-09-09 10:11:21 +0530

    left rotate by d element

--- GeekForgeeks/src/arrays/LeftRotateArray.java
+++ GeekForgeeks/src/arrays/LeftRotateArray.java
@@ -1,15 +1,30 @@
 package arrays;
 
 public class LeftRotateArray {
-	static void leftRoateArray(int[] arr,int d) {
-		for(int i=0;i<0;i++) {
-			LeftrotateAnArrayByOne.leftRotate(arr);
+	static void leftRotateOne(int[] arr) {
+		int n=arr.length;
+		int temp=arr[0];
+		for(int i=0;i<n-1;i++) {
+			arr[i]=arr[i+1];
 		}
+		arr[n-1]=temp;
+//		for(int i=0;i<n;i++) {
+//			System.out.print(arr[i]+" ");
+//		}
 	}
+	static void leftRoateArray(int[] arr,int d) {
+		for(int i=0;i<d;i++) {
+			leftRotateOne(arr);
+		}
+		int n=arr.length;
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array= {8, 5, 9, 20, 9 };
+		int[] array= {8, 5, 9, 20, 10 };
 		leftRoateArray(array,2);
 
 	}

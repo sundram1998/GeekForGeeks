1536e33d8be6cd065a28da1beded0e07e1e1ecbb
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-10-18 20:30:01 +0530

    add

---
+++ GeekForgeeks/src/practiceGeekForGeek/Comptetion.java
@@ -0,0 +1,41 @@
+package practiceGeekForGeek;
+
+public class Comptetion {
+	
+	static void  ab(long a[],long b[]) {
+		int ca=0;
+        int cb=0;
+        // Your code goes here
+        if(a[0]!=b[0]){
+            if(a[0]>b[0]){
+                ca++;
+            }else{
+                cb++;
+            }
+        }
+        if(a[1]!=b[1]){
+            if(a[1]>b[1]){
+                ca++;
+            }else{
+                cb++;
+            }
+        }
+        if(a[2]!=b[2]){
+            if(a[2]>b[2]){
+                ca++;
+            }else{
+                cb++;
+            }
+        }
+        System.out.println(ca+" "+cb);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long[] A = {4, 2, 7};
+		long[] B = {5, 6, 3};
+		ab(A, B); 
+
+	}
+
+}

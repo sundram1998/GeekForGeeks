52ebf23b807ccb69bfc300213f6547ea3eb0a181
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-11-23 08:03:58 +0530

    add codes

---
+++ GeekForgeeks/src/e_searching/SearchInInfiniteArray.java
@@ -0,0 +1,39 @@
+package e_searching;
+
+public class SearchInInfiniteArray {
+	//time complexity O(position)
+	// Naive Solution
+	static int searchInInfiniteArray(int[] arr, int x) {
+		int i = 0;
+		while (true) {
+			if (arr[i] == x)
+				return i;
+			if (arr[i] > x)
+				return -1;
+			i++;
+		}
+	}
+	// Efficient solution
+	// time complexity of efficient solution O(log(position))
+	static int search(int[] arr, int x) {
+		if (arr[0] == x)
+			return 0;
+		int i = 1;
+		while (arr[i] < x) {
+			i = i * 2;
+		}
+		if (arr[i] == x)
+			return i;
+		// we will search from i/2+1 (half of the current index) and i current index
+		return BinarySearch.bSearchRecursive(arr, (i / 2) + 1, i, x);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+		System.out.println(searchInInfiniteArray(arr, 4));
+		System.out.println(search(arr, 5));
+
+	}
+
+}
---
+++ GeekForgeeks/src/e_searching/SearchInSortedRotatedArray.java
@@ -0,0 +1,38 @@
+package e_searching;
+
+public class SearchInSortedRotatedArray {
+
+	static int searchInSortedRotatedArray(int arr[], int x) {
+		int n = arr.length - 1;
+		int low = 0, high = n;
+		while (low <= high) {
+			int mid = (low + high) / 2; // mid=(0+6)/2=3 || low=mid+1==3+1=4==low, high=6
+			if (arr[mid] == x) // mid=4+6=10/2=5 ,high=5,low=4,mid=4 arr[4]==20
+				return mid;
+			if (arr[low] < arr[mid]) {// int arr[] = { 100, 200, 300, 400, 20, 30, 40 };
+				if (x >= arr[low] && x < arr[mid])
+					high = mid - 1;
+				else
+					low = mid + 1;
+			} else {
+				if (x > arr[mid] && x < arr[high])
+					low = mid + 1;
+				else
+					high = mid - 1;
+			}
+
+		}
+		return -1;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = { 100, 200, 300, 400, 20, 30, 40 };
+		int array[] = { 100, 500, 10, 20, 30, 40, 50 };
+		System.out.println(searchInSortedRotatedArray(arr, 20));
+		System.out.println(searchInSortedRotatedArray(array, 500));
+
+	}
+
+}

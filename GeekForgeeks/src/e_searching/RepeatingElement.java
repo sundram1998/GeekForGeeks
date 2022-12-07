a3f67d6d464aabe0ca336873d7582dca5c17b4f0
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-12-07 07:42:55 +0530

    add

--- GeekForgeeks/src/e_searching/RepeatingElement.java
+++ GeekForgeeks/src/e_searching/RepeatingElement.java
@@ -1,6 +1,20 @@
 package e_searching;
 
 public class RepeatingElement {
+	
+	static int repeating(int[] arr) {
+		int n=arr.length-1;
+		boolean[] visited=new boolean[n];
+		for(int i=0;i<n-1;i++) {
+			visited[i]=false;
+		}
+		for(int i=0;i<n;i++) {
+			if(visited[arr[i]])
+				return arr[i];
+			visited[arr[i]]=true;
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

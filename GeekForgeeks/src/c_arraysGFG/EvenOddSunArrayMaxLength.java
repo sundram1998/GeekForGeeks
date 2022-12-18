b82149c1caa4c05a669062ab889a05777d11d41a
SUNDARAM YADAV <SUNDARAM YADAV@DESKTOP-OIINU8J>
2022-12-18 08:56:18 +0530

    update

--- GeekForgeeks/src/c_arraysGFG/EvenOddSunArrayMaxLength.java
+++ GeekForgeeks/src/c_arraysGFG/EvenOddSunArrayMaxLength.java
@@ -18,11 +18,29 @@ public class EvenOddSunArrayMaxLength {
 		}
 		return result;
 	}
+	
+	// efficient maxlength
+	
+	static int maxlength(int[] arr) {
+		int n=arr.length;
+		int res=1;
+		int count=1;
+		for(int i=1;i<n;i++) {
+			if (arr[i]%2==0 && arr[i-1]%2!=0 || arr[i]%2!=0 && arr[i-1]%2==0) {
+				count++;
+				res=Math.max(count, res);
+			}else {
+				count=1;
+			}
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[]= {10,12,14,7,8};
 		System.out.println(maxLengthOddEven(arr));
+		System.out.println(maxlength(arr));
 
 	}
 
